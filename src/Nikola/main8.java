dd52fb21d9249d93c7b87ed26e65cf9fb0e0da9b
Nikola <nik.7377@gmail.com>
2020-10-20 13:51:06 +0200

    task 8 finished

--- src/Nikola/main8.java
+++ src/Nikola/main8.java
@@ -14,12 +14,33 @@ public class main8 {
         int num3 = SCA.nextInt();
 
 
-        //solution with if
+
         if (num1 == num2 && num2 == num3){
             System.out.println("The numbers are the same!");
         }
-        else if (num1 == num2 && num2 > num3){
-            System.out.println("Number 1 and Number 2 are the same and bigger than " + num3);
+        else if (num1 == num2){
+            if (num2 > num3) {
+                System.out.println("Number 1 and Number 2 are the same and bigger than " + num3);
+            }
+            else{
+                System.out.println("Number 1 and Number 2 are the same and smaller than " + num3);
+            }
+        }
+        else if (num2 == num3){
+            if (num3 > num1) {
+                System.out.println("Number 2 and Number 3 are the same and bigger than " + num1);
+            }
+            else{
+                System.out.println("Number 2 and Number 3 are the same and smaller than " + num1);
+            }
+        }
+        else if (num1 == num3){
+            if (num1 > num2) {
+                System.out.println("Number 1 and Number 3 are the same and bigger than " + num2);
+            }
+            else{
+                System.out.println("Number 1 and Number 3 are the same and smaller than " + num2);
+            }
         }
         else if (num1 > num2 && num1 > num3){
             if (num2 > num3) {
@@ -29,16 +50,21 @@ public class main8 {
                 System.out.println("The biggest one is " + num1 + " the 2nd one is " + num3 + ", and the smallest one is " + num2);
             }
         }
-        else if (num2 > num1 && num2 > num3 && num1 > num3){
-            if (num2 > num3) {
-                System.out.println("The biggest one is " + num1 + " the 2nd one is " + num2 + ", and the smallest one is " + num3);
+        else if (num2 > num1 && num2 > num3){
+            if (num1 > num3) {
+                System.out.println("The biggest one is " + num2 + " the 2nd one is " + num1 + ", and the smallest one is " + num3);
             }
-            else if (num2 < num3){
-                System.out.println("The biggest one is " + num1 + " the 2nd one is " + num3 + ", and the smallest one is " + num2);
+            else if (num1 < num3){
+                System.out.println("The biggest one is " + num2 + " the 2nd one is " + num3 + ", and the smallest one is " + num1);
             }
         }
-        else {
-            System.out.println(num3 + " is the biggest number<");
+        else if (num3 > num1 && num3 > num2){
+            if (num1 > num2) {
+                System.out.println("The biggest one is " + num3 + " the 2nd one is " + num1 + ", and the smallest one is " + num2);
+            }
+            else if (num1 < num2){
+                System.out.println("The biggest one is " + num3 + " the 2nd one is " + num2 + ", and the smallest one is " + num1);
+            }
         }
     }
 }
