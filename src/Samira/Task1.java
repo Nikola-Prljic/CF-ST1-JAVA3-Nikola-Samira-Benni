0ec1ce4772d1f0f8e2420943f8cfbae3c5d1ab83
samira khadem <samira.khademhoseini@gmail.com>
2020-10-20 09:51:45 +0200

    firts commit

---
+++ src/Samira/Task1.java
@@ -0,0 +1,15 @@
+package Samira;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print(" ");
+
+
+
+
+    }
+}
